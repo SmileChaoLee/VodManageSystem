fbbd8ef1672c7fe2b092f84cd25769d46457d5ab
SmileChaoLee <leechao1959@gmail.com>
2026-06-22 13:25:42 -0400

    Stop tracking files

--- VodManageSystem/Models/YouTubeModels/VideoItem.cs
+++
@@ -1,8 +0,0 @@
-namespace VodManageSystem.Models.YouTubeModels
-{
-    public class VideoItem
-    {
-        public VideoId Id { get; set; } = null;
-        public VideoSnippet Snippet { get; set; } = null;
-    }
-}
